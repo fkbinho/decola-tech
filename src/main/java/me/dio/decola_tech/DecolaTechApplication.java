0ee398179bfb8e49ad95dca805ccf80560389ee0
fkbinho <sir_kleber@hotmail.com>
2025-03-23 10:31:05 -0300

    Initial commit

---
+++ src/main/java/me/dio/decola_tech/DecolaTechApplication.java
@@ -0,0 +1,13 @@
+package me.dio.decola_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecolaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecolaTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/decola_tech/DecolaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.decola_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
